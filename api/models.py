139cab2ff4c02ff61c3ef33c641c59be92b2137f
MotasemZiad <moatasem.abunema@gmail.com>
2022-09-12 18:33:42 +0300

    polishing the app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+recipe_types = [('BREAKFAST', 'Breakfast'), ('LUNCH', 'Lunch'),
+                ('COFFEE', 'Coffee'), ('DINNER', 'Dinner')]
+
+
+class Restaurant(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=120, unique=True, verbose_name="Name")
+    address = models.CharField(
+        max_length=120, null=True, verbose_name="Address")
+    phone = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=120, unique=True, verbose_name="Name")
+    type = models.CharField(max_length=20, choices=recipe_types)
+    thumbnail = models.ImageField(
+        upload_to="recipe_thumbnails", default="recipe_thumbnails/default.png")
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=120, verbose_name="Name")
+    recipe = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        self.name
---
+++ api/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from .models import (Restaurant, Recipe, Ingredient)
+import base64
+from django.conf import settings
+import os
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['id', 'name', 'address', 'phone']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.SerializerMethodField('encode_thumbnail')
+    ingredients = serializers.SerializerMethodField('get_ingredients')
+
+    def encode_thumbnail(self, recipe):
+        with open(os.path.join(settings.MEDIA_ROOT, recipe.thumbnail.name), "rb") as image_file:
+            return base64.b64encode(image_file.read())
+
+    def get_ingredients(self, recipe):
+        try:
+            recipe_ingredients = Ingredient.objects.filter(recipe_id=recipe.id)
+            return IngredientSerializer(recipe_ingredients, many=True).data
+        except Ingredient.DoesNotExist:
+            return None
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop("ingredient")
+        restaurant = Restaurant.objects.get(pk=validated_data['restaurant_id'])
+        validated_data["restaurant"] = restaurant
+        recipe = Recipe.objects.create(**validated_data)
+
+        if ingredients_data:
+            for ingredient_dict in ingredients_data:
+                ingredient = Ingredient(name=ingredient_dict['name'])
+                ingredient.save()
+                ingredient.recipe.add(recipe)
+        return recipe
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'name', 'type', 'thumbnail', 'ingredients']
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
