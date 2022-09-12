5668e96b17ad6d37a56c0c9d9291eaebf74dd72e
MotasemZiad <moatasem.abunema@gmail.com>
2022-09-12 17:14:05 +0300

    setting up the PostgreSQL database

--- restaurants_demo_drf/settings.py
+++ restaurants_demo_drf/settings.py
@@ -76,8 +76,17 @@ WSGI_APPLICATION = "restaurants_demo_drf.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'restaurants_drf',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
