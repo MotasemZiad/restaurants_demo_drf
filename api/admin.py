9d0d14496e51b1acef9b043462907e3e45c82a00
MotasemZiad <moatasem.abunema@gmail.com>
2022-09-13 15:17:58 +0300

    some updates

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import (Restaurant, Recipe, Ingredient)
 # Register your models here.
+
+
+admin.site.register(Restaurant)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
